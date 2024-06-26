a12487a93a9fc8e397a1e74eebc62cad2f98d279
Non T <117605943+Ratanon691@users.noreply.github.com>
2024-06-26 22:33:57 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-// write code here
+function fruit() {
+  let basket = {}
+  while (true) {
+    let key = prompt(`what kind of fruit is in the basket?`)
+    if (key.toLowerCase() === `stop`) {
+      break;
+    }
+    let value = prompt(`how many ${key} are there?`)
+    if(value > 1){
+      basket[key + `s`] = value
+    }else{
+    basket[key] = value
+    }
+  }
+  console.log(basket)
+}
